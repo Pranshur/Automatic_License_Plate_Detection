512c054a202dfd7ac2b13975b0dcf38abb1763d9
Pranshur <69423571+Pranshur@users.noreply.github.com>
2022-01-24 20:45:42 +0530

    Add files via upload

---
+++ Automatic License Plate Detection.py
@@ -0,0 +1,20 @@
+import cv2
+
+plateCascade=cv2.CascadeClassifier('./haarcascades/haarcascade_russian_plate_number.xml')
+minArea=500
+
+vid=cv2.VideoCapture(0)
+
+while True:
+    ret, frame=vid.read()
+    imgGray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    plates=plateCascade.detectMultiScale(imgGray, 1.1, 4)
+    for (x, y, w, h) in plates:
+        area=w*h
+        if area>minArea:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            cv2.putText(frame, "Number Plate", (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), thickness=2)
+
+    cv2.imshow('Automatic License Plate Detection', frame)
+    cv2.waitKey(1)
